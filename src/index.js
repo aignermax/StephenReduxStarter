6fae69110617e08af13eb707894d54b5f14696ec
BUREAU\max_a <max_aigneraigner@web.de>
2017-06-30 10:44:27 +0200

    Added first steps to Stephens tutorial

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyAQ1Mzto_81gp0zNVTnyUPnVCfePCt1lZQ';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+    console.log(data);
+});
+
+
+// Create a new component. This component should produce 
+// some HTML
+const App = () => {
+    return (
+    <div>
+        <SearchBar/>
+    </div>
+    );
+}
+
+// Take this component's generated HTML and put it 
+// on the page (in the  DOM)
+
+
+ReactDOM.render(<App /> , document.querySelector('.container'));
